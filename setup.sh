72ce29cf9e24c94e92e42b87355c93a73d538fa4
Amr Ali <amr.ali.cc@gmail.com>
2014-04-06 04:12:30 +0200

    Refactor link installation and add 'etc' to TOPDIRS

--- setup.sh
+++ setup.sh
@@ -3,14 +3,19 @@
 # So we can call this from anywhere
 BASE=$(readlink -e $(dirname $0))
 
+# List of top dirs
+TOPDIRS=(bin etc)
+
 # Make sure that all submodules are initialized and updated
 git submodule update --init
 
 # Install links to all dot files and folders
-for fd in $BASE/userdots/.[A-Z]*; do
-    ln -sf `readlink -e $fd` $HOME/
+for fname in $BASE/userdots/.[A-Z]*; do
+    ln -sf `readlink -e $fname` $HOME/
 done
 
 # Install links for the top-dirs
-ln -sf `readlink -e $BASE/bin` $HOME/
+for dir in ${TOPDIRS[*]}; do
+    ln -sf `readlink -e $BASE/$dir` $HOME/
+done
 
